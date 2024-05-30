633a65bee052acad767a13ec39542ab6a46c18e5
Adam Hebert <adam.hebert656@gmail.com>
2024-05-30 12:40:11 -0500

    Add arrow.nvim and which-key entries

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>T"] = { name = "ToggleTerm" },
+  ["<leader>h"] = { name = "arrow" },
+  ["<leader>hh"] = { "<cmd>Arrow open<cr>", "File Bookmarks Menu" },
+  ["<leader>hm"] = { "<cmd>lua require('arrow.buffer_ui').openMenu()<cr>", "Buffer Bookmarks Menu" },
+})
---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,8 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  opts = {
+    show_icons = true,
+    leader_key = '<leader>hh', -- Recommended to be a single key
+    buffer_leader_key = '<leader>hm', -- Per Buffer Mappings
+  }
+}
