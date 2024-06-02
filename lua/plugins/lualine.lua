c3d5154394f77f181dba8721ea1d2db053683f6b
Adam Hebert <adam.hebert656@gmail.com>
2024-06-02 11:09:35 -0500

    Rebasinf

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+local icons = require("lazyvim.config").icons
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = {
+    sections = {
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path() }
+      }
+    }
+  }
+}
+
+
