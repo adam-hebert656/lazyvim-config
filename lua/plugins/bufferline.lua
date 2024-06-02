fedf0eb89e9f703d49a3935f055009ee291950d2
Adam Hebert <adam.hebert656@gmail.com>
2024-06-02 12:43:26 -0500

    Configure Bufferline to always show

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/bufferline.nvim',
+  opts = {
+    options = {
+      always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+
+    }
+  }
+}
