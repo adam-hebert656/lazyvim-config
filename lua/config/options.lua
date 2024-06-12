bb4b4b8d69840fde34b12dfc0f7fb609e8f483d2
Adam Hebert <adam.hebert656@gmail.com>
2024-06-12 16:55:31 -0500

    Commit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,4 @@
 -- Add any additional options here
 
 vim.g.autoformat = false
+vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
--- lua/plugins/project.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ahmedkhalf/project.nvim",
-  opts = {
-    manual_mode = false,
-    detection_methods = { "lsp", "pattern" },
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs" },
-  }
-}
